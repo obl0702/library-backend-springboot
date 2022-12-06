5ba6bff410910073c10eb7854b626cf0fcb4ccdc
boonloong <obl0702@gmail.com>
2022-12-06 16:56:38 +0800

    Initial Version

---
+++ src/main/java/com/ongbl/librarybackendspringboot/LibraryBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ongbl.librarybackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBackendSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ongbl/librarybackendspringboot/LibraryBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongbl.librarybackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
