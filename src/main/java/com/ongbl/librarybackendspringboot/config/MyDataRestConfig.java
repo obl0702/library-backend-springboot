29c9e9a2856428aebf4e14cec6a6dedcb3497851
boonloong <obl0702@gmail.com>
2022-12-13 15:35:56 +0800

    add review DAO

--- src/main/java/com/ongbl/librarybackendspringboot/config/MyDataRestConfig.java
+++ src/main/java/com/ongbl/librarybackendspringboot/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.ongbl.librarybackendspringboot.config;
 
 import com.ongbl.librarybackendspringboot.entity.Book;
+import com.ongbl.librarybackendspringboot.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -18,8 +19,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
---
+++ src/main/java/com/ongbl/librarybackendspringboot/dao/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.ongbl.librarybackendspringboot.dao;
+
+import com.ongbl.librarybackendspringboot.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+
+}
